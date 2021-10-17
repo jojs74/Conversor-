77f5796aac01fb7a2a60f8256ae40812cc6ad910
jojs74 <jjs74@outlook.com>
2021-10-17 19:55:26 -0300

    primeiro commit conversor

---
+++ conversor.py
@@ -0,0 +1,6 @@
+# Conversão de Graus Fahrenheit para Celsius
+
+f = float(input('digite a temperatura em Fahrenheit:'))
+c = (f - 32) * 5/9
+
+print(f'A temperatura em graus fahrenheit convertida para graus Celsius é:{c:.2f}')
